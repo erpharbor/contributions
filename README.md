f43bca2123a3deb60228b98829d93b4e74e68f29
Sudhir Arya <sudhir@erpharbor.com>
2018-02-23 14:22:56 +0530

    [ADD] Added module to extend database manager screen layout.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.org/erpharbor/contributions.svg?branch=10.0)](https://travis-ci.org/erpharbor/contributions)
 # contribution
 Contribution
---
+++ web_extend_db_manager_layout/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Web Extend Database Manager Layout',
+    'summary': 'Web Extend Database Manager Layout',
+    'author': 'ERP Harbor Consulting Services',
+    'website': 'http://www.erpharbor.com',
+    'category': 'web',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'web',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
