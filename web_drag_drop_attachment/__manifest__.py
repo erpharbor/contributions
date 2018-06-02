6ffe04ce3162c79705dee3f30a57cde77d7af2d7
Sudhir Arya <sudhir@erpharbor.com>
2018-06-02 15:46:14 +0530

    [ADD] Added web module to allow user to drag-drop the files to create attachment

---
+++ web_drag_drop_attachment/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Web Drag Drop Attachment',
+    'version': '10.0.1.0.0',
+    'category': 'Web',
+    'summary': 'Upload the documents in attachments by drag and drop.',
+    'author': 'ERP Harbor Consulting Services',
+    'website': 'http://www.erpharbor.com',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+}
