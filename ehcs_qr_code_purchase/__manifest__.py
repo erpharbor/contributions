0bf5c696c61760d1b1ee14a5e596a37aedb02158
Harsh Savsani <harsh.savsani.erpharbor@gmail.com>
2018-11-12 15:00:14 +0530

    [ADD] Add QR Code modules for version 11.

---
+++ ehcs_qr_code_purchase/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'QR Code Purchase',
+    'version': '11.0.1.0.0',
+    'category': 'Purchase',
+    'author': 'ERP Harbor Consulting Services',
+    'summary': 'Generate QR Code for Purchase',
+    'website': 'http://www.erpharbor.com',
+    'description': """""",
+    'depends': [
+        'ehcs_qr_code_base',
+        'purchase',
+    ],
+    'data': [
+        'report/purchase_order_report_template.xml',
+        'views/qr_code_purchase_view.xml',
+    ],
+    'images': [
+        'static/description/banner.png',
+    ],
+    'installable': True,
+}
