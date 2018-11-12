b127ba3f1b1385fcd888be5a4ef517fb68a224b0
Harsh Savsani <harsh.savsani.erpharbor@gmail.com>
2018-11-12 15:28:46 +0530

    [ADD] Add QR Code modules for version 12.

--- ehcs_qr_code_invoice/__manifest__.py
+++ ehcs_qr_code_invoice/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'QR Code Invoice',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting',
     'author': 'ERP Harbor Consulting Services',
     'summary': 'Generate QR Code for Invoice',
@@ -9,7 +9,7 @@
     'description': """""",
     'depends': [
         'ehcs_qr_code_base',
-        'account_invoicing',
+        'account',
     ],
     'data': [
         'report/account_invoice_report_template.xml',
