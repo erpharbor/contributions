a1b71b73573a339dfad91b07b38f19656eaa73f9
Sudhir Arya <sudhir@erpharbor.com>
2018-12-18 17:06:44 +0530

    [ADD] Added web_extend_db_manager_layout module in v11

---
+++ web_extend_db_manager_layout/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Web Extend Database Manager Layout',
+    'summary': 'Web Extend Database Manager Layout',
+    'author': 'ERP Harbor Consulting Services',
+    'website': 'http://www.erpharbor.com',
+    'category': 'web',
+    'version': '11.0.1.0.0',
+    'depends': [
+        'web',
+    ],
+    'images': [
+        'static/description/banner.png',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
